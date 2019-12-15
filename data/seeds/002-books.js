45c5666e1e57421652be83b3eab5a85fa3736100
sooshe <sooshe@sooshebot.com>
2019-12-15 16:43:59 -0600

    update books seed file

--- data/seeds/002-books.js
+++ data/seeds/002-books.js
@@ -1,13 +1,14 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('books').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('books').insert([
+        {author_id:'', book_title:'', book_genre:''},
+        {author_id:'', book_title:'', book_genre:''},
+        {author_id:'', book_title:'', book_genre:''},
+        {author_id:'', book_title:'', book_genre:''}
       ]);
     });
 };
