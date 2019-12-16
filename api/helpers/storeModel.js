b96fbfcb6428df7a34a789e9152fa167ebd1a1fd
sooshe <sooshe@sooshebot.com>
2019-12-16 11:13:12 -0600

    update storemodel and router content

--- api/helpers/storeModel.js
+++ api/helpers/storeModel.js
@@ -9,3 +9,34 @@ module.exports = {
   update,
   remove
 };
+
+function get() {
+  return db('stores');
+}
+
+function getById(id) {
+  return db('stores')
+    .where({ id })
+    .first();
+}
+
+function getByAuthor() {}
+
+function getByBook() {}
+
+function add(store) {
+  return db('stores').insert(store);
+}
+
+function update(changes, id) {
+  return db('stores', 'id')
+    .where({ id })
+    .update(changes, '*')
+    .then(count => getById(id));
+}
+
+function remove(id) {
+  return db('stores')
+  .where({id})
+  .del();
+};
